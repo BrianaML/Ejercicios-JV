553bdbfe3978daac57133733ab26a77d6aa32b2b
BrianaML <bmikkelsen-loth@udesa.edu.ar>
2024-05-15 00:01:10 -0300

    detalles

--- js/funciones.js
+++ js/funciones.js
@@ -4,13 +4,16 @@ function rectangulo(altura, ancho) {
 }
 
 console.log(rectangulo(8,9));
-//rectangulo(10,8)
 console.log(rectangulo(10,8));
 
-function triangulo(base, altura, division) {
+
+
+function triangulo(base, altura) {
     let area= (base * altura)/2
     return area
 }
+
+
 
 console.log(triangulo(2,3));
 
@@ -21,6 +24,8 @@ function largoDelArray(array) {
 let dulces= ["coockies", "cinnamons", "brownies", "croassaints"]
 console.log(largoDelArray(dulces));
 
+
+
 function candidadDeLetras(palabra) {
     return palabra.length
 }
@@ -28,14 +33,16 @@ function candidadDeLetras(palabra) {
 let word= "chocolinas"
 console.log(candidadDeLetras(word));
 
-function dolarHoy(precioArg) {
-    let tipoCambio= 1000
-    let precioDolar= precioArgH / tipoCambio;
-    return precioEnDolares
-}
 
-let pesos = 4000;
-//console.log(dolarHoy(precioArgH));
+
+function dolarHoy(precioPesosArgentinos){
+    let factorCambio = 1000
+        return precioPesosArgentinos * factorCambio
+}
+let enDolares = dolarHoy(50)
+console.log(enDolares)
+
+
 
 function precioFinal(precio) {
     let precioIva = precio * 1.21
@@ -45,16 +52,51 @@ function precioFinal(precio) {
 let precioProducto = 5000
 console.log(precioFinal(precioProducto));
 
+
+
 function mitad(numero) {
     return numero / 2
 }
 
 console.log(mitad(50));
 
-function diaSegunNumero(array, numero) {
-    let diasSemana= ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
-    let nu
-    return diaSegunNumero[diasSemana];
+
+
+function diaSegunNumero(array, numero){
+    let resultadoArray = array;
+    let numeroDia = numero;
+    return resultadoArray[numeroDia]
 }
-let 
-//console.log(diaSegunNumero);
+
+let resultado = diaSegunNumero(['19/08/24', '10/08/24', '11/04/24', '3/02/24'], 1)
+console.log(resultado)
+
+//ejecutando funciones
+
+function siguiente(numero) {
+    let anterior =numero + 1
+    return anterior
+}
+
+let valores= siguiente(5)
+console.log(valores);
+
+
+function cuadriple(numero) {
+    let cuadriple = numero * 4
+    return cuadriple;
+}
+
+let masDeCuatro = cuadriple(5)
+console.log(masDeCuatro);
+
+
+function siguienteDelCuadriple(numero) {
+    let siguienteDelCuadriple = siguiente(cuadriple(numero))
+    return siguienteDelCuadriple
+}
+
+let sigueinteDeCuatro = siguienteDelCuadriple (8)
+console.log(sigueinteDeCuatro);
+
+
